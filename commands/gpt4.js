a606898faea3558de078b4634b309227cff0bd34
Abdallah226 <143345865+Abdallah226@users.noreply.github.com>
2025-04-18 23:34:19 +0000

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,7 +1,7 @@
 {
-  "operator": "Abdallah",
-  "question": "Salut",
-  "response": "Salut ! Je suis le chat bot de sir Abdallah, je suis ravi de faire votre connaissance !",
-  "timestamp": "2025-04-18T23:25:05.736Z",
+  "operator": "Ronald Sory",
+  "question": "Yo",
+  "response": "Bonjour ! Je suis Ronald Bot, le système de conversation créé par Ronald Sory. Je suis là pour discuter et répondre à vos questions. Je note que vous n'avez pas demandé l'heure actuelle... Mais je la dirai quand vous le demanderez !",
+  "timestamp": "2025-04-18T23:30:45.032Z",
   "powered_by": "Ronald IA - LLaMA 3 via Groq"
 }
