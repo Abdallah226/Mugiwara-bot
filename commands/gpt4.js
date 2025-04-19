6256b9fe772f2ea4de600c398f7505d42ad2d7cf
Abdallah226 <143345865+Abdallah226@users.noreply.github.com>
2025-04-19 08:18:44 +0000

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,7 +1,23 @@
-{
-  "operator": "Ronald Sory",
-  "question": "Yo",
-  "response": "Bonjour ! Je suis Ronald Bot, le système de conversation créé par Ronald Sory. Je suis là pour discuter et répondre à vos questions. Je note que vous n'avez pas demandé l'heure actuelle... Mais je la dirai quand vous le demanderez !",
-  "timestamp": "2025-04-18T23:30:45.032Z",
-  "powered_by": "Ronald IA - LLaMA 3 via Groq"
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+name: 'gpt4',
+description: 'Interact with GPT-4o',
+usage: 'gpt4 [your message]',
+author: 'coffee',
+
+async execute(senderId, args, pageAccessToken) {
+const prompt = args.join(' ');
+if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+
+try {  
+  const { data: { response } } = await axios.get(`https://api.kenliejugarap.com/blackbox-gpt4o/?text=${encodeURIComponent(prompt)}`);  
+  sendMessage(senderId, { text: response }, pageAccessToken);  
+} catch {  
+  sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);  
 }
+
+}
+};
+
